d46e6c42ce4dbee8120879b89589bd99b04a097d
daizhan <daizhan@live.cn>
2017-08-16 22:27:56 +0800

    公共弹出层开发-完成

--- svg-draw-optimization/js/common.js
+++ svg-draw-optimization/js/common.js
@@ -206,7 +206,7 @@
     })(cobj);
 
     /**
-     * common popup
+     * color picker
      */
     (function(cobj) {
         cobj.ColorPicker = {
@@ -351,6 +351,83 @@
                 this.setEvent(callback);
             }
         }
+    })(cobj);
+
+    (function(cobj) {
+        cobj.Menu = {
+            name: "menu-popup",
+            selector: ".menu-popup",
+            createDom: function(data) {
+                var liHtml = "",
+                    html = "",
+                    className = data.type || "",
+                    item;
+                for (var i = 0, len = data.menus.length; i < len; i++) {
+                    item = data.menus[i];
+                    if (item.type == "separator") {
+                        liHtml += '<li class="separator"></li>';
+                    } else {
+                        liHtml += '<li class="' + item.operate + ' ' + item.status + '" data-operate="' + item.operate + '" data-value="' + item.value + '"><i></i><span>' + item.text + '</span><span class="shortcut">' + item.shortcut + '</span></li>';
+                    }
+                }
+                html = '<div class="common-popup-block menu-popup ' + className + '">\
+                            <ul>' + liHtml + '</ul>\
+                        </div>';
+                $("body").append(html);
+            },
+            setPopupPos: function($target) {
+                var $popup = $(this.selector);
+                if ($target instanceof $) {
+                    var offset = $target.offset(),
+                        height = $target.height(),
+                        gap = 5;
+                    $popup.css({
+                        left: offset.left + "px",
+                        top: offset.top + height + gap + "px"
+                    });
+                } else { // 应该传递一个事件对象
+                    $popup.css({
+                        left: $target.clientX + "px",
+                        top: $target.clientY + "px"
+                    });
+                }
+            },
+            clean: function() {
+                var $popup = $(this.selector);
+                $popup.remove();
+                this.triggerType = "";
+                $("body").off("click", this.bindBodyClickEvent);
+            },
+            setEvent: function(callback) {
+                var self = this;
+                var $popup = $(this.selector);
+                $popup.find("li").click(function() {
+                    var $target = $(this),
+                        operate = $target.attr("data-operate"),
+                        value = $target.attr("data-value");
+                    if ($target.hasClass("disabled") || $target.hasClass("separator")) {
+                        return;
+                    }
+                    callback(operate, value);
+                    self.clean();
+                });
+                $("body").on("click", self.bindBodyClickEvent);
+            },
+            bindBodyClickEvent: function(event) {
+                var $target = $(event.target);
+                if (cobj.Menu.triggerType != "triggerByTarget" && !$target.hasClass(cobj.Menu.name) && !$target.parents(cobj.Menu.name).length) {
+                    cobj.Menu.clean();
+                }
+                cobj.Menu.triggerType = "";
+            },
+            init: function($target, data, callback, triggerType) {
+                this.clean();
+                this.triggerType = triggerType;
+                this.createDom(data);
+                this.setPopupPos($target);
+                this.setEvent(callback);
+            }
+        };
     })(cobj);
 
     /**
--- svg-draw-optimization/js/draw.js
+++ svg-draw-optimization/js/draw.js
@@ -6,4 +6,85 @@
             C.layer.topNotify("info", { content: "颜色值: #" + color, shade: false, time: 2 });
         }, "triggerByTarget");
     });
+    var data = {
+        type: "with-selected",
+        menus: [{
+                operate: "copy",
+                status: "selected",
+                value: "",
+                text: "复制",
+                shortcut: "ctrl+c"
+            },
+            {
+                operate: "copy",
+                status: "disabled",
+                value: "",
+                text: "复制",
+                shortcut: "ctrl+c"
+            },
+            {
+                type: "separator"
+            },
+            {
+                operate: "cut",
+                status: "",
+                value: "cut",
+                text: "剪切",
+                shortcut: "ctrl+x"
+            },
+            {
+                operate: "page",
+                status: "",
+                value: "paste",
+                text: "粘贴",
+                shortcut: "ctrl+v"
+            }
+        ]
+    };
+    var data1 = {
+        type: "",
+        menus: [{
+                operate: "copy",
+                status: "selected",
+                value: "",
+                text: "复制",
+                shortcut: "ctrl+c"
+            },
+            {
+                operate: "copy",
+                status: "disabled",
+                value: "",
+                text: "复制",
+                shortcut: "ctrl+c"
+            },
+            {
+                type: "separator"
+            },
+            {
+                operate: "cut",
+                status: "",
+                value: "cut",
+                text: "剪切",
+                shortcut: "ctrl+x"
+            },
+            {
+                operate: "page",
+                status: "",
+                value: "paste",
+                text: "粘贴",
+                shortcut: "ctrl+v"
+            }
+        ]
+    };
+    $(".border-width, .border-style").click(function(event) {
+        var $target = $(this);
+        C.Menu.init($target, data, function(operate, value) {
+            C.layer.topNotify("info", { content: "operate: " + operate + "<br />value: " + value, shade: false, time: 2 });
+        }, "triggerByTarget");
+    });
+    $(".draw-content").click(function(event) {
+        C.Menu.init(event, data1, function(operate, value) {
+            C.layer.topNotify("info", { content: "operate: " + operate + "<br />value: " + value, shade: false, time: 2 });
+        }, "triggerByTarget");
+    });
 })();
