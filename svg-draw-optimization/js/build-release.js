8328d43dea8bc325a75b96ec858ec54eeb744d45
daizhan <daizhan@live.cn>
2017-08-18 18:16:41 +0800

    require.js 打包配置优化

---
+++ svg-draw-optimization/js/build-release.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: ".",
+    mainConfigFile: "config.js",
+    paths: {
+        "jquery": "empty:",
+        "underscore": "empty:",
+        "backbone": "empty:",
+        "svg": "empty:"
+    },
+    name: "draw",
+    out: "main.js",
+    generateSourceMaps: true
+})
